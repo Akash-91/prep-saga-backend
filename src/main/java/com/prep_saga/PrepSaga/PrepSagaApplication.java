0cc772010c5e6b42b460708899dee87f6a2f56e9
Akash <akash.pyal22@gmail.com>
2025-08-03 17:15:49 +0200

    init added

--- src/main/java/com/prep_saga/PrepSaga/PrepSagaApplication.java
+++ src/main/java/com/prep_saga/PrepSaga/PrepSagaApplication.java
@@ -1,5 +1,6 @@
 package com.prep_saga.PrepSaga;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,4 +11,9 @@ public class PrepSagaApplication {
 		SpringApplication.run(PrepSagaApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init() {
+		System.out.println(">>> JWT_SECRET from env: " + System.getenv("JWT_SECRET"));
+		System.out.println(">>> Active profile: " + System.getProperty("spring.profiles.active"));
+	}
 }
